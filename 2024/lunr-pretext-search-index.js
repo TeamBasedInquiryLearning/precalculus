6719d8b845e6d4eb0ce057eefc046c47601ae67c
Drew Lewis <30658947+siwelwerd@users.noreply.github.com>
2023-09-13 19:43:42 +0000

    Latest build deployed.

---
+++ 2024/lunr-pretext-search-index.js
@@ -0,0 +1,193 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "colophon",
+  "level": "1",
+  "url": "colophon.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": "  Precalculus for Team-Based Inquiry Learning   copyright  "
+},
+{
+  "id": "PR1",
+  "level": "1",
+  "url": "PR1.html",
+  "type": "Section",
+  "number": "1.1",
+  "title": "Graphing Quadratic Functions (PR1)",
+  "body": " Graphing Quadratic Functions (PR1)     Graph quadratic functions and identify their axis of symmetry, and maximum or minimum point.      Activities  Activities go here! Don't forget to put text in <p> tags or it won't show up.    Activities may start with an <introduction> .   Then we can ask students to do some <task> s.  Here's a second <task> .     Videos  It would be great to include videos down here, like in the Calculus book!   "
+},
+{
+  "id": "objectives-2",
+  "level": "2",
+  "url": "PR1.html#objectives-2",
+  "type": "Objectives",
+  "number": "1.1",
+  "title": "",
+  "body": "   Graph quadratic functions and identify their axis of symmetry, and maximum or minimum point.    "
+},
+{
+  "id": "activity-1",
+  "level": "2",
+  "url": "PR1.html#activity-1",
+  "type": "Activity",
+  "number": "1.1.1",
+  "title": "",
+  "body": "  Activities may start with an <introduction> .   Then we can ask students to do some <task> s.  Here's a second <task> .  "
+},
+{
+  "id": "PR2",
+  "level": "1",
+  "url": "PR2.html",
+  "type": "Section",
+  "number": "1.2",
+  "title": "Quadratic Models and Meanings (PR2)",
+  "body": " Quadratic Models and Meanings (PR2)     Use quadratic models to solve an application problem and establish conclusions.      Activities  Activities go here! Don't forget to put text in <p> tags or it won't show up.    Activities may start with an <introduction> .   Then we can ask students to do some <task> s.  Here's a second <task> .     Videos  It would be great to include videos down here, like in the Calculus book!   "
+},
+{
+  "id": "objectives-3",
+  "level": "2",
+  "url": "PR2.html#objectives-3",
+  "type": "Objectives",
+  "number": "1.2",
+  "title": "",
+  "body": "   Use quadratic models to solve an application problem and establish conclusions.    "
+},
+{
+  "id": "activity-2-1",
+  "level": "2",
+  "url": "PR2.html#activity-2-1",
+  "type": "Activity",
+  "number": "1.2.1",
+  "title": "",
+  "body": "  Activities may start with an <introduction> .   Then we can ask students to do some <task> s.  Here's a second <task> .  "
+},
+{
+  "id": "PR3",
+  "level": "1",
+  "url": "PR3.html",
+  "type": "Section",
+  "number": "1.3",
+  "title": "Polynomial Long Division (PR3)",
+  "body": " Polynomial Long Division (PR3)     Use polynomial long division to rewrite a rational function as a polynomial plus a proper rational function.      Activities  Activities go here! Don't forget to put text in <p> tags or it won't show up.    Activities may start with an <introduction> .   Then we can ask students to do some <task> s.  Here's a second <task> .     Videos  It would be great to include videos down here, like in the Calculus book!   "
+},
+{
+  "id": "objectives-4",
+  "level": "2",
+  "url": "PR3.html#objectives-4",
+  "type": "Objectives",
+  "number": "1.3",
+  "title": "",
+  "body": "   Use polynomial long division to rewrite a rational function as a polynomial plus a proper rational function.    "
+},
+{
+  "id": "activity-3-1",
+  "level": "2",
+  "url": "PR3.html#activity-3-1",
+  "type": "Activity",
+  "number": "1.3.1",
+  "title": "",
+  "body": "  Activities may start with an <introduction> .   Then we can ask students to do some <task> s.  Here's a second <task> .  "
+},
+{
+  "id": "PR4",
+  "level": "1",
+  "url": "PR4.html",
+  "type": "Section",
+  "number": "1.4",
+  "title": "Zeroes of Polynomial Functions (PR4)",
+  "body": " Zeroes of Polynomial Functions (PR4)     Determine the zeros of a real polynomial function, write a polynomial function given information about its zeros and their multiplicities, and apply the Factor Theorem and the Fundamental Theorem of Algebra.      Activities  Activities go here! Don't forget to put text in <p> tags or it won't show up.    Activities may start with an <introduction> .   Then we can ask students to do some <task> s.  Here's a second <task> .     Videos  It would be great to include videos down here, like in the Calculus book!   "
+},
+{
+  "id": "objectives-5",
+  "level": "2",
+  "url": "PR4.html#objectives-5",
+  "type": "Objectives",
+  "number": "1.4",
+  "title": "",
+  "body": "   Determine the zeros of a real polynomial function, write a polynomial function given information about its zeros and their multiplicities, and apply the Factor Theorem and the Fundamental Theorem of Algebra.    "
+},
+{
+  "id": "activity-4-1",
+  "level": "2",
+  "url": "PR4.html#activity-4-1",
+  "type": "Activity",
+  "number": "1.4.1",
+  "title": "",
+  "body": "  Activities may start with an <introduction> .   Then we can ask students to do some <task> s.  Here's a second <task> .  "
+},
+{
+  "id": "PR5",
+  "level": "1",
+  "url": "PR5.html",
+  "type": "Section",
+  "number": "1.5",
+  "title": "Graphs of Polynomial Functions (PR5)",
+  "body": " Graphs of Polynomial Functions (PR5)     Find the intercepts and end behavior of a polynomial function, and use this information to sketch its graph. I can use this information along with a sign chart to solve polynomial inequalities.      Activities  Activities go here! Don't forget to put text in <p> tags or it won't show up.    Activities may start with an <introduction> .   Then we can ask students to do some <task> s.  Here's a second <task> .     Videos  It would be great to include videos down here, like in the Calculus book!   "
+},
+{
+  "id": "objectives-6",
+  "level": "2",
+  "url": "PR5.html#objectives-6",
+  "type": "Objectives",
+  "number": "1.5",
+  "title": "",
+  "body": "   Find the intercepts and end behavior of a polynomial function, and use this information to sketch its graph. I can use this information along with a sign chart to solve polynomial inequalities.    "
+},
+{
+  "id": "activity-5-1",
+  "level": "2",
+  "url": "PR5.html#activity-5-1",
+  "type": "Activity",
+  "number": "1.5.1",
+  "title": "",
+  "body": "  Activities may start with an <introduction> .   Then we can ask students to do some <task> s.  Here's a second <task> .  "
+},
+{
+  "id": "PR6",
+  "level": "1",
+  "url": "PR6.html",
+  "type": "Section",
+  "number": "1.6",
+  "title": "Properties of Graphs of Rational Functions (PR6)",
+  "body": " Properties of Graphs of Rational Functions (PR6)     Use the formula of a rational function to find the domain and range, vertical and horizontal asymptotes, and intercepts. I can use this i nformation along with a sign chart to sketch the graph of a given rational function.      Activities  Activities go here! Don't forget to put text in <p> tags or it won't show up.    Activities may start with an <introduction> .   Then we can ask students to do some <task> s.  Here's a second <task> .     Videos  It would be great to include videos down here, like in the Calculus book!   "
+},
+{
+  "id": "objectives-7",
+  "level": "2",
+  "url": "PR6.html#objectives-7",
+  "type": "Objectives",
+  "number": "1.6",
+  "title": "",
+  "body": "   Use the formula of a rational function to find the domain and range, vertical and horizontal asymptotes, and intercepts. I can use this i nformation along with a sign chart to sketch the graph of a given rational function.    "
+},
+{
+  "id": "activity-6-1",
+  "level": "2",
+  "url": "PR6.html#activity-6-1",
+  "type": "Activity",
+  "number": "1.6.1",
+  "title": "",
+  "body": "  Activities may start with an <introduction> .   Then we can ask students to do some <task> s.  Here's a second <task> .  "
+},
+{
+  "id": "colophon-2",
+  "level": "1",
+  "url": "colophon-2.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": " This book was authored in PreTeXt .  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
