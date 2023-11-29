760bcc50e3f72d7461b28d2359a4d984a31d7ddc
Steven Clontz <steven.clontz@gmail.com>
2023-11-29 20:33:55 +0000

    Latest build deployed.

--- 2024/lunr-pretext-search-index.js
+++ 2024/lunr-pretext-search-index.js
@@ -19,9 +19,9 @@ var ptx_lunr_docs = [
   "body": " Graphing Quadratic Functions (PR1)     Graph quadratic functions and identify their axis of symmetry, and maximum or minimum point.      Activities  Quadratic functions have many different applications in the real world. For example, say we want to identify a point at which the maximum profit or minimum cost occurs. Before we can interpret some of these situations, however, we will first need to understand how to read the graphs of quadratic functions to locate these least and greatest values.    Use the graph of the quadratic function to answer the questions below.    Quadratic function that opens downward      How would you describe the behavior of this function? What is happening to the -values as the -values increase? Do you notice any other patterns of the -values of the table?        0    1    2    3    4    5     At which point does the graph reach it’s maximum or minimum value? How can you tell from the graph that this is the maximum or minimum value?  Look at the function given and the graph of the function. What do you notice? Is there a faster way to find the maximum or minimum value from the given function?      The vertex form of a quadratic function is given by , where is the vertex of the parabola and is the axis of symmetry.      Use the given the quadratic function, , to answer the following:   Apply the definition to find the vertex of the parabola and the axis of symmetry.  Compare what you got in part with the values you found in the previous activity. What do you notice?     Given the standard form of a quadratic function, , with real coefficients , and , the axis of symmetry is defined as and has a vertex at the point .      Use the graph of the quadratic function to answer the questions below.    Quadratic function that opens downward with vertex (-1,4)      Which of the following quadratic functions matches the graph shown in the figure?              What is the maximum or minimum value?                 Consider the following four graphs of quadratic functions:                     Which of the graphs above have a maximum?  Graph A  Graph B  Graph C  Graph D    Which of the graphs above have an axis of symmetry of ?  Graph A  Graph B  Graph C  Graph D    Which of the graphs above represents the function ?  Graph A  Graph B  Graph C  Graph D    Which of the graphs above represents the function ?  Graph A  Graph B  Graph C  Graph D       Notice that the maximum or minimum value of the quadratic function is the vertex. How can you determine if the vertex is a maximum or minimum?      Sketch the graph of a function that meets the following criteria:    The function has a maximum at 7.  The axis of symmetry is at .     Videos  It would be great to include videos down here, like in the Calculus book!   "
 },
 {
-  "id": "objectives-2",
+  "id": "PR1-2",
   "level": "2",
-  "url": "PR1.html#objectives-2",
+  "url": "PR1.html#PR1-2",
   "type": "Objectives",
   "number": "1.1",
   "title": "",
@@ -109,9 +109,9 @@ var ptx_lunr_docs = [
   "body": " Quadratic Models and Meanings (PR2)     Use quadratic models to solve an application problem and establish conclusions.      Activities  Activities go here! Don't forget to put text in <p> tags or it won't show up.    A water balloon is tossed vertically from a fifth story window. It's height , in meters, at a time , in seconds, is modeled by the function    Complete the following table.        0    1    2    3    4    5      Explain why is not a linear function.  What is the meaning of ?  the initial height of the water balloon is 25 meters.  the water balloon reaches a maximum height of 25 meters.  the water balloon hits the ground after 25 seconds.  the water balloon travels 25 meters before hitting the ground.   Find the vertex of the quadratic function.              What is the meaning of the vertex?  The water balloon reaches a maximum height of 25 meters at the start.  After 2 seconds, the water balloon reaches a maximum height of 45 meters.  After 5 seconds, the water balloon reaches a maximum height.  After 1 second, the water balloon reaches a maximum height of 40 meters.       The population of a small city is given by the function , where is the number of years after 2015.  \\ When will the population of the city reach a maximum?              Determine when the population of the city is increasing and when it is decreasing.  When will the population of the city reach 36,000 people?                 The unit price of an item affects its supply and demand. That is, if the unit price goes up, the demand for the item will usually decrease. For example, an online streaming service currently has 84 million subscribers at a monthly charge of $6. Market research has suggested that if the owners raise the price to $8, they would lose 4 million subscribers. Assume that subscriptions are linearly related to the price.  \\ Which of the following represents a linear function which relates the price of the streaming service to the number of subscribers ?              Using the fact that Revenue , which of the following represents the Revenue in terms of the price .              What price should the streaming service charge for a monthly subscription to maximize their revenue?              How many subscribers would the company have at this price?  39.5 million  48 million  57 million  76 million       Videos  It would be great to include videos down here, like in the Calculus book!   "
 },
 {
-  "id": "objectives-3",
+  "id": "PR2-2",
   "level": "2",
-  "url": "PR2.html#objectives-3",
+  "url": "PR2.html#PR2-2",
   "type": "Objectives",
   "number": "1.2",
   "title": "",
@@ -127,18 +127,18 @@ var ptx_lunr_docs = [
   "body": "  A water balloon is tossed vertically from a fifth story window. It's height , in meters, at a time , in seconds, is modeled by the function    Complete the following table.        0    1    2    3    4    5      Explain why is not a linear function.  What is the meaning of ?  the initial height of the water balloon is 25 meters.  the water balloon reaches a maximum height of 25 meters.  the water balloon hits the ground after 25 seconds.  the water balloon travels 25 meters before hitting the ground.   Find the vertex of the quadratic function.              What is the meaning of the vertex?  The water balloon reaches a maximum height of 25 meters at the start.  After 2 seconds, the water balloon reaches a maximum height of 45 meters.  After 5 seconds, the water balloon reaches a maximum height.  After 1 second, the water balloon reaches a maximum height of 40 meters.    "
 },
 {
-  "id": "activity-7",
+  "id": "PR2-3-4",
   "level": "2",
-  "url": "PR2.html#activity-7",
+  "url": "PR2.html#PR2-3-4",
   "type": "Activity",
   "number": "1.2.3",
   "title": "",
   "body": "  The population of a small city is given by the function , where is the number of years after 2015.  \\ When will the population of the city reach a maximum?              Determine when the population of the city is increasing and when it is decreasing.  When will the population of the city reach 36,000 people?              "
 },
 {
-  "id": "activity-8",
+  "id": "PR2-3-5",
   "level": "2",
-  "url": "PR2.html#activity-8",
+  "url": "PR2.html#PR2-3-5",
   "type": "Activity",
   "number": "1.2.4",
   "title": "",
@@ -154,9 +154,9 @@ var ptx_lunr_docs = [
   "body": " Polynomial Long Division (PR3)     Rewrite a rational function as a polynomial plus a proper rational function.      Activities    Using long division, find the quotient and remainder for the given rational function. Rewrite the function as a polynomial plus a proper rational function, given .   What is the quotient?  What is the remainder?  What is the divisor?  Write the rational function as a polynomial plus a proper rational function.  How can you check your answer? (Hint: Think of regular long division with positive integers.)     Videos  It would be great to include videos down here, like in the Calculus book!   "
 },
 {
-  "id": "objectives-4",
+  "id": "PR3-2",
   "level": "2",
-  "url": "PR3.html#objectives-4",
+  "url": "PR3.html#PR3-2",
   "type": "Objectives",
   "number": "1.3",
   "title": "",
@@ -181,27 +181,27 @@ var ptx_lunr_docs = [
   "body": " Zeroes of Polynomial Functions (PR4)     Determine the zeros of a real polynomial function, write a polynomial function given information about its zeros and their multiplicities, and apply the Factor Theorem and the Fundamental Theorem of Algebra.       Factor Theorem   A number c is a zero of a polynomial function f(x) if and only if x-c is a factor of f(x).     Fundamental Theorem of Algebra   A polynomial function f of degree n > 0 has at least one zero.    Activities    Write the polynomial function in factored form using information from the graph below.        Using the given graph, what are the real zeros of this function? Select all that apply. 0  1  -3  3  4  -4   What are the least possible multiplicities for each zero?  What is the least degree of the function? 3  4  5  6   Describe the end behavior of the graph.  As ,  As ,  As ,  As ,   Combining the information from the end behavior with the degree of the function, will the leading coefficient be positive or negative?  positive  negative   Given the point (2, ) is on the curve, and using the information in parts (a) through (e), write the function for the graph above in factored form.     Given the function ,   Find all the zeros and their corresponding multiplicities.  Write the function in factored form using linear factors.     Given the function ,   Find all the zeros and their corresponding multiplicities.  Write the function in factored form using linear factors.     The zeros of a function are x=2, with multiplicity 1, x=-1, with multiplicity 2 and x=i.   Given the information above, find a polynomial function with real coefficients of least degree.     Videos  It would be great to include videos down here, like in the Calculus book!   "
 },
 {
-  "id": "objectives-5",
+  "id": "PR4-2",
   "level": "2",
-  "url": "PR4.html#objectives-5",
+  "url": "PR4.html#PR4-2",
   "type": "Objectives",
   "number": "1.4",
   "title": "",
   "body": "   Determine the zeros of a real polynomial function, write a polynomial function given information about its zeros and their multiplicities, and apply the Factor Theorem and the Fundamental Theorem of Algebra.    "
 },
 {
-  "id": "theorem-1",
+  "id": "PR4-3-1",
   "level": "2",
-  "url": "PR4.html#theorem-1",
+  "url": "PR4.html#PR4-3-1",
   "type": "Theorem",
   "number": "1.4.1",
   "title": "Factor Theorem.",
   "body": " Factor Theorem   A number c is a zero of a polynomial function f(x) if and only if x-c is a factor of f(x).   "
 },
 {
-  "id": "theorem-2",
+  "id": "PR4-3-2",
   "level": "2",
-  "url": "PR4.html#theorem-2",
+  "url": "PR4.html#PR4-3-2",
   "type": "Theorem",
   "number": "1.4.2",
   "title": "Fundamental Theorem of Algebra.",
@@ -253,9 +253,9 @@ var ptx_lunr_docs = [
   "body": " Graphs of Polynomial Functions (PR5)     Find the intercepts, estimated locations of maxima and minima, and end behavior of a polynomial function, and use this information to sketch the graph.      Activities  A polynomial function is a function that can be expressed in the form of a polynomial. Just like other functions, polynomial functions have many different features. Before we can begin to look at how polynomial functions can be used, we must first be able to identify what makes a polynomial function and what typical characteristics they have.    Some of the graphs shown below are polynomial functions and some are not. Use the following graphs to explore the charactertistics of polynomials.                     By looking at the graphs that are labeled \"NOT a Polynomial Function,\" what type of characteristics do you notice? How are these different from the graphs labeled as being polynomial functions?  Can you make any conjectures about what characteristics all polynomials have based on what you see on these graphs?      There are two primary characteristics we use to distinguish polynomial functions from other functions. Polynomial functions have graphs that are smooth and continuous. Smooth functions are functions that contain only rounded curves (no sharp corners). Continuous functions are functions that can be drawn without lifting your pencil (no breaks).      Now that we know what polynomial functions look like, we should be able to determine some characteristics. Use the graph below to find the given charactertistics.    Polynomial Function      What are the -intercept(s) of the polynomial function? Select all that apply.              What are the -intercept(s) of the polynomial function?             How many zeros does this polynomial function have?  0  1  2  3   At what point is the local minimum located?                  At what point is the local maximum located?                  How do you describe the behavior of the polynomial function as ?  the -values go to negative infinity     the -values go to positive infinity      How do you describe the behavior of the polynomial function as ?  the -values go to negative infinity     the -values go to positive infinity         Sketch the graph of a function that meets all of the following criteria. Be sure to scale your axes and label any important features of your graph.   The -intercepts of are 0, 2, and 5.  has one maximum at 0. has one minimum at -5 and another at -16.  The end behavior of is given as: As ,  As ,     Videos  It would be great to include videos down here, like in the Calculus book!   "
 },
 {
-  "id": "objectives-6",
+  "id": "PR5-2",
   "level": "2",
-  "url": "PR5.html#objectives-6",
+  "url": "PR5.html#PR5-2",
   "type": "Objectives",
   "number": "1.5",
   "title": "",
@@ -307,9 +307,9 @@ var ptx_lunr_docs = [
   "body": " Properties and Graphs of Rational Functions (PR6)     Find the domain and range, vertical and horizontal asymptotes, and intercepts of a rational function and use this information to sketch the graph.      Activities    A function is rational rational function provided that it is possible to write as the ratio of two polynomials, and . That is, is rational provided that for some polynomial functions and , we have       Consider the rational function    Find , , and .  One answer above gave us some information about the domain of . Which one? Why?  Another answer gave us some information about the zeros of . Which one? Why?  Another answer gave us some information about a point on the graph of that is not a zero. Which one? Why?     Let and be polynomial functions so that is a rational function. The domain domain of a rational function of is the set of all real numbers except those for which .      Consider the following six graphs of rational functions:                           Which of the graphs above represents the function ?  Which of the graphs above represents the function ?     Videos  It would be great to include videos down here, like in the Calculus book!   "
 },
 {
-  "id": "objectives-7",
+  "id": "PR6-2",
   "level": "2",
-  "url": "PR6.html#objectives-7",
+  "url": "PR6.html#PR6-2",
   "type": "Objectives",
   "number": "1.6",
   "title": "",
@@ -352,9 +352,9 @@ var ptx_lunr_docs = [
   "body": "  Consider the following six graphs of rational functions:                           Which of the graphs above represents the function ?  Which of the graphs above represents the function ?  "
 },
 {
-  "id": "colophon-2",
+  "id": "backmatter-2",
   "level": "1",
-  "url": "colophon-2.html",
+  "url": "backmatter-2.html",
   "type": "Colophon",
   "number": "",
   "title": "Colophon",
