3e0b90e11daab06f767f897551cc703ff784fb7c
Steven Clontz <steven.clontz@gmail.com>
2023-11-29 20:33:29 +0000

    update build script

--- scripts/bank/build.py
+++ scripts/bank/build.py
@@ -2,6 +2,10 @@ from checkit.bank import Bank
 import os
 
 generate_images = input("Generate images? y/n ").strip().upper() == "Y"
+if generate_images:
+    print("regenerating images")
+else:
+    print("skipping images")
 os.chdir("exercises")
 b = Bank()
 b.generate_exercises(images=generate_images)
