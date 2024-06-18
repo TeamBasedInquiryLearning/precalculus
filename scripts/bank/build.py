0d304673d451e47d34edd052cd66cf26998bbe57
Steven Clontz <steven.clontz@gmail.com>
2024-06-18 16:19:46 +0000

    run checkit on build

--- scripts/bank/build.py
+++ scripts/bank/build.py
@@ -1,11 +1,12 @@
 from checkit.bank import Bank
 import os
 
-generate_images = input("Generate images? y/n ").strip().upper() == "Y"
-if generate_images:
-    print("regenerating images")
-else:
-    print("skipping images")
+# generate_images = input("Generate images? y/n ").strip().upper() == "Y"
+# if generate_images:
+#     print("regenerating images")
+# else:
+#     print("skipping images")
+generated_images = False
 os.chdir("exercises")
 b = Bank()
 b.generate_exercises(images=generate_images)
