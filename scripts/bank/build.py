d2b1663577e2b95452f36022f07fa83488cde7e2
Steven Clontz <steven.clontz@gmail.com>
2024-06-20 04:31:47 +0000

    bank build script moves into stage

--- scripts/bank/build.py
+++ scripts/bank/build.py
@@ -1,5 +1,6 @@
-from checkit.bank import Bank
 import os
+import shutil
+from checkit.bank import Bank
 
 # generate_images = input("Generate images? y/n ").strip().upper() == "Y"
 # if generate_images:
@@ -13,3 +14,4 @@ b.generate_exercises(images=generate_images)
 b.write_json()
 b.build_viewer()
 os.chdir("..")
+shutil.copytree("exercises/docs", "output/stage/2024/exercises", dirs_exist_ok=True)
