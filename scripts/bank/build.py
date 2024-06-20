6909c86e0fc09bbaf7912a170ecef9514dc1010d
Steven Clontz <steven.clontz@gmail.com>
2024-06-20 08:17:46 -0700

    run checkit in workflow (#346)
    * run checkit on build
    
    * fixes
    
    * try again
    
    * try build.container
    
    * rename test-and-deploy
    
    * try build.container
    
    * install deps
    
    * try running cloudflare outside container
    
    * fix yaml
    
    * download artifact
    
    * order of operations
    
    * rename to build-and-deploy
    
    * full build
    
    * fix target name
    
    * upgrade pretext
    
    * add xmlid for problematic image
    
    * and to others
    
    * fix cludgy checkit deploy
    
    * bank build script moves into stage
    
    * bump ptx version

--- scripts/bank/build.py
+++ scripts/bank/build.py
@@ -1,14 +1,17 @@
-from checkit.bank import Bank
 import os
+import shutil
+from checkit.bank import Bank
 
-generate_images = input("Generate images? y/n ").strip().upper() == "Y"
-if generate_images:
-    print("regenerating images")
-else:
-    print("skipping images")
+# generate_images = input("Generate images? y/n ").strip().upper() == "Y"
+# if generate_images:
+#     print("regenerating images")
+# else:
+#     print("skipping images")
+generate_images = False
 os.chdir("exercises")
 b = Bank()
 b.generate_exercises(images=generate_images)
 b.write_json()
 b.build_viewer()
 os.chdir("..")
+shutil.copytree("exercises/docs", "output/stage/2024/exercises", dirs_exist_ok=True)
